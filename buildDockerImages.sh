0432db1403fc37c085f06fa416303d79da60d426
Bruno Rosell i Gui <rosell@iiia.csic.es>
2024-08-16 17:02:39 +0200

    Fix build c&P error

--- buildDockerImages.sh
+++ buildDockerImages.sh
@@ -7,21 +7,21 @@ else
 	pushd $DIR > /dev/null
 	VERSION=$(grep --max-count=1 '<version>' pom.xml | awk -F '>' '{ print $2 }' | awk -F '<' '{ print $1 }')
 	FILE_DATE=$(date -r src/dev/docker/Dockerfile +%s)
-	IMAGE_DATE=$(docker inspect --format='{{.Created}}' valawai/c0_email_sensor:dev 2>/dev/null)
+	IMAGE_DATE=$(docker inspect --format='{{.Created}}' valawai/c0_email_actuator:dev 2>/dev/null)
 	if [ $? -ne 0 ]; then
 		IMAGE_DATE=0
 	else
 		IMAGE_DATE=$(echo $IMAGE_DATE|date +%s)
 	fi
 	if [ $FILE_DATE -ge $IMAGE_DATE ]; then
-		DOCKER_BUILDKIT=1 docker build -f src/dev/docker/Dockerfile -t valawai/c0_email_sensor:dev .
+		DOCKER_BUILDKIT=1 docker build -f src/dev/docker/Dockerfile -t valawai/c0_email_actuator:dev .
 	fi
 	TAG=${1:-$VERSION}
-	DOCKER_PARAMS="--rm --name c0_email_sensor_build_docker_image --add-host=host.docker.internal:host-gateway -v /var/run/docker.sock:/var/run/docker.sock"
+	DOCKER_PARAMS="--rm --name c0_email_actuator_build_docker_image --add-host=host.docker.internal:host-gateway -v /var/run/docker.sock:/var/run/docker.sock"
 	if [[ "$OSTYPE" == "darwin"* ]]; then
 		DOCKER_PARAMS="$DOCKER_PARAMS -e TESTCONTAINERS_HOST_OVERRIDE=docker.for.mac.host.internal"
 	fi
-	docker run $DOCKER_PARAMS -v "${HOME}/.m2":/root/.m2  -v "${PWD}":/app valawai/c0_email_sensor:dev ./mvnw clean package -DskipTests -Dquarkus.container-image.tag=$TAG
+	docker run $DOCKER_PARAMS -v "${HOME}/.m2":/root/.m2  -v "${PWD}":/app valawai/c0_email_actuator:dev ./mvnw clean package -DskipTests -Dquarkus.container-image.tag=$TAG
 	if [ $? -ne 0 ]; then
 		echo "Cannot build docker image"
 		popd > /dev/null
